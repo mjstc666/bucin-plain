c17cb79a3e4629e27e9de281184f01d3ba39448f
mjstc666 <jafar8d20@gmail.com>
2020-11-30 20:52:33 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,13 +7,13 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
-    { quote: "I love you.", author: "Brian" },
-    { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "I'm sorry for loving you.", author: "Jafar" },
+    { quote: "I love you.", author: "Jafar" },
+    { quote: "Aku sayang kamu!", author: "Jafar" },
+    { quote: "Jafar sayang Eltita ♥", author: "Jafar-nya Eltita!" },
+    { quote: "Love uuu Baby", author: "Jafar" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["Pulpyy", "Eltitakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
