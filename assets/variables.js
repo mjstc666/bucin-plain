f305dd1f221ea25c5738b72216baaf90fde45095
mjstc666 <75250658+mjstc666@users.noreply.github.com>
2020-11-30 19:23:58 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -10,10 +10,10 @@ const variables = {
     { quote: "I'm sorry for loving you.", author: "standinhsd" },
     { quote: "I love you.", author: "Brian" },
     { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "Jafar sayang Eltita ♥", author: "Ian-nya Ava!" },
+    { quote: "Love uuu", author: "Bribri" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["Pulpyy", "Ayangkuu", "Sayang", "Baby", "Cintaku", "Cinta"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
